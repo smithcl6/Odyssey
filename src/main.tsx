63912d1597c3e86f0e26b33266b7a71d160884c5
Christopher Smith <smithcl6@alumni.vcu.edu>
2025-07-11 02:40:22 -0400

    Added Material UI to the project.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GlobalStyles, StyledEngineProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <StyledEngineProvider enableCssLayer>
+            <GlobalStyles styles="@layer theme, base, mui, components, utlilities;" />
+            <App />
+        </StyledEngineProvider>
+    </StrictMode>,
 )
