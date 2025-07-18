149c8ff2668014c9adee9d12a9fa5fe3f0b21cf2
Christopher Smith <smithcl6@alumni.vcu.edu>
2025-07-17 21:30:29 -0400

    Theme selection displays icons instead of text.

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,22 +1,49 @@
-import { useContext } from "react";
+import { useContext, type JSX } from "react";
 import { ThemeContext } from "./ThemeContext";
 import AudioManager from "./AudioManager";
+import { CiDark } from "react-icons/ci";
+import { GiTriforce, GiSuperMushroom } from "react-icons/gi";
+import { MenuItem, Select } from "@mui/material";
+
+interface ThemeIcon {
+    key: string,
+    icon: JSX.Element
+}
 
 function Navbar() {
     const themeContext = useContext(ThemeContext);
 
-    const themes = ['dark', 'zelda', 'mario'];
+    const themes: ThemeIcon[] = [
+        {
+            key: 'dark',
+            icon: <CiDark />
+        },
+        {
+            key: 'zelda',
+            icon: <GiTriforce />
+        },
+        {
+            key: 'mario',
+            icon: <GiSuperMushroom />
+        },
+    ];
+
     const options = themes.map(
-        theme => <option key={theme} value={theme} className="bg-emerald-500">{theme}</option>
+        theme => <MenuItem className="flex justify-center text-white hover:bg-gray-600 text-4xl" key={theme.key} value={theme.key}>{theme.icon}</MenuItem>
     );
 
     return (
         <header className="flex justify-between p-4 sticky top-0 z-10 bg-gray-900">
             <h1>Current theme: { themeContext.theme }</h1>
-            <label htmlFor="themes"></label>
-            <select id="themes" onChange={(value) => themeContext.setTheme(value.target.value)}>
+            <Select
+                className="text-white bg-gray-800 text-4xl rounded-2xl"
+                name="Theme"
+                value={themeContext.theme}
+                onChange={(value) => themeContext.setTheme(value.target.value)}
+                notched
+            >
                 {options}
-            </select>
+            </Select>
             <AudioManager></AudioManager>
         </header>
     )
