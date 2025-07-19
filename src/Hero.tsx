54fe487d909b2e316ceca5b9e4a14111f1a8ed45
Christopher Smith <smithcl6@alumni.vcu.edu>
2025-07-18 21:15:38 -0400

    Hero page has external links and more dynamic text sizing. Also added resume.

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,14 +1,27 @@
+import { FaGithub, FaLinkedin, FaFilePdf } from "react-icons/fa";
+
 function Hero() {
     return (
-        <section className="hero flex p-4 pt-20 sm:p-24 bg-cover bg-center sm:h-screen">
+        <section className="hero flex p-4 pt-24 sm:p-24 bg-cover bg-center min-h-screen">
             <div className="flex flex-col">
-                <h1 className="text-6xl p-2 font-bold text-purple-400 zelda:text-amber-500 mario:text-red-500">Christopher Smith</h1>
-                <h2 className="text-4xl p-2 font-semibold text-blue-500">Software Developer</h2>
-                <p className="text-2xl p-2 font-medium">
+                <h1 className="text-4xl sm:text-6xl p-2 font-bold text-purple-400 zelda:text-amber-500 mario:text-red-500">Christopher Smith</h1>
+                <h2 className="text-2xl sm:text-4xl p-2 font-semibold text-blue-400">Software Developer</h2>
+                <p className="text-2xl sm:text-4xl p-2 font-medium">
                     I'm a junior developer with full-stack experience, especially in front-end development using Angular and React.
                     With that said, I am still early in my career and am open-minded to areas outside of web dev.
                     In any case, I care about writing clean, maintainable, and scalable code.
                 </p>
+                <span className="flex text-6xl sm:text-9xl justify-around flex-wrap p-4 pt-8">
+                    <a className="hero-links hover:text-purple-700" href='https://github.com/smithcl6' title="GitHub" target='_blank' rel='noopener noreferrer'>
+                        <FaGithub /><h1 className="text-xl sm:text-4xl">GitHub</h1>
+                    </a>
+                    <a className="hero-links hover:text-blue-600" href='https://www.linkedin.com/in/csmith1414/' title="Linkedin" target='_blank' rel='noopener noreferrer'>
+                        <FaLinkedin /><h1 className="text-xl sm:text-4xl">Linkedin</h1>
+                    </a>
+                    <a className="hero-links hover:text-emerald-600" href='src/assets/resume.pdf' title="Resume" target='_blank' rel='noopener noreferrer'>
+                        <FaFilePdf /><h1 className="text-xl sm:text-4xl">Resume</h1>
+                    </a>
+                </span>
             </div>
         </section>
     );
