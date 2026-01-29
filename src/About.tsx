ceb49c122f9e8386b604693d4cf56f3b9b65c043
Christopher Smith <smithcl6@alumni.vcu.edu>
2026-01-28 20:11:27 -0500

    Updated profile picture and formatted About Me Section. About Me section still needs content and is not being rendered yet.

--- src/About.tsx
+++ src/About.tsx
@@ -1,29 +1,26 @@
+import Profile from '/src/assets/profile.jpg';
+
 function About() {
     return (
         <section>
-            <h2 className='font-semibold'>More About Me.</h2>
-            <p className='mt-10'>
-                Professional Stuff Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Sed nec rhoncus velit. Aenean sit amet dolor
-                vulputate, laoreet lorem non, rhoncus massa. Donec est ante,
-                maximus in lectus sit amet, accumsan lacinia mi. Fusce cursus
-                sapien eu faucibus sollicitudin. Phasellus eget enim vitae risus
-                vehicula euismod. Donec mattis lorem ac tempus fermentum. Nulla
-                facilisi. Aliquam dapibus pulvinar hendrerit. Fusce ipsum
-                libero, ornare in molestie ac, dictum vitae lorem. Nulla vel
-                pretium augue, nec hendrerit sapien.
-            </p>
-            <p className='mt-10'>
-                Personal Stuff Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Sed nec rhoncus velit. Aenean sit amet dolor
-                vulputate, laoreet lorem non, rhoncus massa. Donec est ante,
-                maximus in lectus sit amet, accumsan lacinia mi. Fusce cursus
-                sapien eu faucibus sollicitudin. Phasellus eget enim vitae risus
-                vehicula euismod. Donec mattis lorem ac tempus fermentum. Nulla
-                facilisi. Aliquam dapibus pulvinar hendrerit. Fusce ipsum
-                libero, ornare in molestie ac, dictum vitae lorem. Nulla vel
-                pretium augue, nec hendrerit sapien.
-            </p>
+            <h2 className='text-theme p-4 font-semibold'>More About Me</h2>
+            <div className='flex flex-col sm:flex-row'>
+                <img
+                    className='w-full rounded-4xl object-scale-down p-4 sm:w-1/2'
+                    src={Profile}
+                />
+                <p className='text-theme p-4 sm:w-1/2'>
+                    Personal Stuff Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Sed nec rhoncus velit. Aenean sit amet
+                    dolor vulputate, laoreet lorem non, rhoncus massa. Donec est
+                    ante, maximus in lectus sit amet, accumsan lacinia mi. Fusce
+                    cursus sapien eu faucibus sollicitudin. Phasellus eget enim
+                    vitae risus vehicula euismod. Donec mattis lorem ac tempus
+                    fermentum. Nulla facilisi. Aliquam dapibus pulvinar
+                    hendrerit. Fusce ipsum libero, ornare in molestie ac, dictum
+                    vitae lorem. Nulla vel pretium augue, nec hendrerit sapien.
+                </p>
+            </div>
         </section>
     );
 }
