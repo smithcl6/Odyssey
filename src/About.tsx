caaec7ddf4f17bf579735077042170526e18cd86
Christopher Smith <smithcl6@alumni.vcu.edu>
2026-01-28 22:05:21 -0500

    Preventing profile image from being too large in About Me.

--- src/About.tsx
+++ src/About.tsx
@@ -6,10 +6,10 @@ function About() {
             <h2 className='text-theme p-4 font-semibold'>More About Me</h2>
             <div className='flex flex-col sm:flex-row'>
                 <img
-                    className='w-full rounded-4xl object-scale-down p-4 sm:w-1/2'
+                    className='m-auto max-h-[80vh] rounded-4xl object-scale-down p-4 sm:max-w-1/2'
                     src={Profile}
                 />
-                <p className='text-theme p-4 sm:w-1/2'>
+                <h4 className='text-theme p-4'>
                     Personal Stuff Lorem ipsum dolor sit amet, consectetur
                     adipiscing elit. Sed nec rhoncus velit. Aenean sit amet
                     dolor vulputate, laoreet lorem non, rhoncus massa. Donec est
@@ -19,7 +19,7 @@ function About() {
                     fermentum. Nulla facilisi. Aliquam dapibus pulvinar
                     hendrerit. Fusce ipsum libero, ornare in molestie ac, dictum
                     vitae lorem. Nulla vel pretium augue, nec hendrerit sapien.
-                </p>
+                </h4>
             </div>
         </section>
     );
