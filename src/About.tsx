0d50e9a047a779303ff6c1165a98cf51eb667e42
Christopher Smith <smithcl6@alumni.vcu.edu>
2026-02-20 17:23:25 -0500

    More styling to About section.

--- src/About.tsx
+++ src/About.tsx
@@ -4,22 +4,28 @@ function About() {
     return (
         <section>
             <h2 className='text-theme p-4 font-semibold'>More About Me</h2>
-            <div className='flex flex-col sm:flex-row'>
+            <div className='bg-color-1 flex flex-col rounded-2xl sm:flex-row'>
                 <img
                     className='m-auto max-h-[80vh] rounded-4xl object-scale-down p-4 sm:max-w-1/2'
                     src={Profile}
                 />
-                <h4 className='text-theme p-4'>
-                    Personal Stuff Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Sed nec rhoncus velit. Aenean sit amet
-                    dolor vulputate, laoreet lorem non, rhoncus massa. Donec est
-                    ante, maximus in lectus sit amet, accumsan lacinia mi. Fusce
-                    cursus sapien eu faucibus sollicitudin. Phasellus eget enim
-                    vitae risus vehicula euismod. Donec mattis lorem ac tempus
-                    fermentum. Nulla facilisi. Aliquam dapibus pulvinar
-                    hendrerit. Fusce ipsum libero, ornare in molestie ac, dictum
-                    vitae lorem. Nulla vel pretium augue, nec hendrerit sapien.
-                </h4>
+                <span className='text-theme m-2 rounded-2xl p-4 sm:m-0'>
+                    <h4>
+                        Professional Stuff Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Sed nec rhoncus velit.
+                        Aenean sit amet dolor vulputate, laoreet lorem non,
+                        rhoncus massa. Donec est ante, maximus in lectus sit
+                        amet, accumsan lacinia mi. Fusce cursus sapien eu
+                        faucibus sollicitudin. Phasellus eget enim vitae risus
+                        vehicula euismod. Donec mattis lorem ac tempus
+                        fermentum. Nulla facilisi. Aliquam dapibus pulvinar
+                        hendrerit. Fusce ipsum libero, ornare in molestie ac,
+                        dictum vitae lorem. Nulla vel pretium augue, nec
+                        hendrerit sapien.
+                    </h4>
+                    <br />
+                    <h4>Hobbies placeholder</h4>
+                </span>
             </div>
         </section>
     );
